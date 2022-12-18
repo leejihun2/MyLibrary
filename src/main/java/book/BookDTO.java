ba0e5e618e969706664de5be4224e625e2c35216
leejohun <yi10286@gmail.com>
2022-12-18 16:08:25 +0900

    Merge remote-tracking branch 'origin/main' into main

---
+++ src/main/java/book/BookDTO.java
@@ -0,0 +1,13 @@
+package book;
+
+public class BookDTO {
+	
+	/*
+	 * book_code varchar2(10) primary key BOOK_TITLE varchar2(100) NOT NULL
+	 * BOOK_AUTHOR varchar2(100) NOT NULL BOOK_GENRE varchar2(5) NOT NULL
+	 * BOOK_STATUS varchar2(5) NOT NULL PUBLISHER varchar2(100) NOT NULL
+	 * PUBLISH_DATE DATE NOT NULL REGI_USER varchar2(20) NOT null REGI_DATE DATE
+	 * DEFAULT sysdate NOT NULL UPDT_USER varchar2(20) UPDT_DATE date
+	 */
+
+}
